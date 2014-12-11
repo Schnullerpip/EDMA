f189c2458ebee541118952cd74460c7a0f153a94
Julian Mayer <julmayer@htwg-konstanz.de>
2014-12-11 10:59:14 +0100

    Fixed #1. Fehler beim Clean-up in Datenbankskript behoben

--- 02_Entwurf/re_create_EDMA.sql
+++ 02_Entwurf/re_create_EDMA.sql
@@ -10,74 +10,13 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+01:00";
 
-
+-- clean up
+DROP DATABASE IF EXISTS `EDMA`;
 --
 -- Datenbank: `EDMA`
 --
-CREATE DATABASE IF NOT EXISTS `EDMA` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE `EDMA` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `EDMA`;
-
-
--- clean up
-
-ALTER TABLE `anhang`
-DROP FOREIGN KEY fk_anhang_projekt;
-
---
--- Foreign keys der Tabelle `datentyp`
---
-
---
--- FOREIGN KEYs der Tabelle `messreihe`
---
-
-ALTER TABLE `messreihe`
-DROP FOREIGN KEY `fk_messreihe_projekt`;
-
---
--- Foreign keys der Tabelle `messreihe_metainfo`
---
-
-ALTER TABLE `messreihe_metainfo`
-DROP FOREIGN KEY fk_messreihe_metainfo_messreihe,
-DROP FOREIGN KEY fk_messreihe_metainfo_metainfo;
-
---
--- Foreign keys der Tabelle `messreihe_sensor`
---
-
-ALTER TABLE `messreihe_sensor`
-DROP FOREIGN KEY fk_messreihe_sensor_messreihe,
-DROP FOREIGN KEY fk_messreihe_sensor_sensor;
-
---
--- Foreign keys der Tabelle `messung`
---
-
-ALTER TABLE `messung`
-DROP FOREIGN KEY fk_messung_messreihe,
-DROP FOREIGN KEY fk_messung_sensor;
-
---
--- Foreign keys der Tabelle `metainfo`
---
-
-ALTER TABLE `metainfo`
-DROP FOREIGN KEY fk_metainfo_datentyp;
-
---
--- Foreign keys der Tabelle `passwort`
---
-
-ALTER TABLE `passwort`
-DROP FOREIGN KEY fk_passwort_projekt;
-
---
--- Foreign keys der Tabelle `projekt`
---
-
-ALTER TABLE `projekt`
-DROP FOREIGN KEY fk_projekt_passwort;
 
 -- --------------------------------------------------------
 
@@ -85,8 +24,7 @@ DROP FOREIGN KEY fk_projekt_passwort;
 -- Tabellenstruktur für Tabelle `anhang`
 --
 
-DROP TABLE IF EXISTS `anhang`;
-CREATE TABLE IF NOT EXISTS `anhang` (
+CREATE TABLE `anhang` (
 `id` bigint(20) unsigned NOT NULL,
   `projekt_id` bigint(20) unsigned NOT NULL,
   `dateiname` varchar(256) NOT NULL,
@@ -96,7 +34,8 @@ CREATE TABLE IF NOT EXISTS `anhang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `anhang`
-ADD CONSTRAINT pk_anhang PRIMARY KEY (id);
+ADD CONSTRAINT pk_anhang PRIMARY KEY (id),
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
 
 -- --------------------------------------------------------
 
@@ -104,15 +43,15 @@ ADD CONSTRAINT pk_anhang PRIMARY KEY (id);
 -- Tabellenstruktur für Tabelle `datentyp`
 --
 
-DROP TABLE IF EXISTS `datentyp`;
-CREATE TABLE IF NOT EXISTS `datentyp` (
+CREATE TABLE `datentyp` (
 `id` bigint(20) unsigned NOT NULL,
   `typ` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `datentyp`
 ADD CONSTRAINT pk_datentyp PRIMARY KEY (id),
-ADD CONSTRAINT unique_typ UNIQUE (typ);
+ADD CONSTRAINT unique_typ UNIQUE (typ),
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
 
 -- --------------------------------------------------------
 
@@ -120,14 +59,14 @@ ADD CONSTRAINT unique_typ UNIQUE (typ);
 -- Tabellenstruktur für Tabelle `messreihe`
 --
 
-DROP TABLE IF EXISTS `messreihe`;
-CREATE TABLE IF NOT EXISTS `messreihe` (
+CREATE TABLE `messreihe` (
 `id` bigint(20) unsigned NOT NULL,
   `projekt_id` bigint(20) unsigned NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `messreihe`
-ADD CONSTRAINT pk_messreihe PRIMARY KEY (id);
+ADD CONSTRAINT pk_messreihe PRIMARY KEY (id),
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
 
 -- --------------------------------------------------------
 
@@ -135,8 +74,7 @@ ADD CONSTRAINT pk_messreihe PRIMARY KEY (id);
 -- Tabellenstruktur für Tabelle `messreihe_metainfo`
 --
 
-DROP TABLE IF EXISTS `messreihe_metainfo`;
-CREATE TABLE IF NOT EXISTS `messreihe_metainfo` (
+CREATE TABLE `messreihe_metainfo` (
   `messreihe_id` bigint(20) unsigned NOT NULL,
   `metainfo_id` bigint(20) unsigned NOT NULL,
   `metawert` varchar(50) NOT NULL
@@ -151,8 +89,7 @@ ADD CONSTRAINT pk_messreihe_metainfo PRIMARY KEY (messreihe_id, metainfo_id);
 -- Tabellenstruktur für Tabelle `messreihe_sensor`
 --
 
-DROP TABLE IF EXISTS `messreihe_sensor`;
-CREATE TABLE IF NOT EXISTS `messreihe_sensor` (
+CREATE TABLE `messreihe_sensor` (
   `messreihe_id` bigint(20) unsigned NOT NULL,
   `sensor_id` bigint(20) unsigned NOT NULL,
   `anzeigename` varchar(30) NOT NULL
@@ -167,8 +104,7 @@ ADD CONSTRAINT pk_messreihe_sensor PRIMARY KEY (messreihe_id, sensor_id);
 -- Tabellenstruktur für Tabelle `messung`
 --
 
-DROP TABLE IF EXISTS `messung`;
-CREATE TABLE IF NOT EXISTS `messung` (
+CREATE TABLE `messung` (
   `messreihe_id` bigint(20) unsigned NOT NULL,
   `sensor_id` bigint(20) unsigned NOT NULL,
   `zeitpunkt` int(10) unsigned NOT NULL,
@@ -184,8 +120,7 @@ ADD CONSTRAINT pk_messung PRIMARY KEY (messreihe_id, sensor_id, zeitpunkt);
 -- Tabellenstruktur für Tabelle `metainfo`
 --
 
-DROP TABLE IF EXISTS `metainfo`;
-CREATE TABLE IF NOT EXISTS `metainfo` (
+CREATE TABLE `metainfo` (
 `id` bigint(20) unsigned NOT NULL,
   `metaname` varchar(30) NOT NULL,
   `datentyp_id` bigint(20) unsigned NOT NULL
@@ -193,7 +128,8 @@ CREATE TABLE IF NOT EXISTS `metainfo` (
 
 ALTER TABLE `metainfo`
 ADD CONSTRAINT pk_metaindo PRIMARY KEY (id),
-ADD CONSTRAINT unique_metaname UNIQUE (metaname);
+ADD CONSTRAINT unique_metaname UNIQUE (metaname),
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
 
 -- --------------------------------------------------------
 
@@ -201,15 +137,15 @@ ADD CONSTRAINT unique_metaname UNIQUE (metaname);
 -- Tabellenstruktur für Tabelle `passwort`
 --
 
-DROP TABLE IF EXISTS `passwort`;
-CREATE TABLE IF NOT EXISTS `passwort` (
+CREATE TABLE `passwort` (
 `id` bigint(20) unsigned NOT NULL,
   `hash` varbinary(256) NOT NULL,
   `projekt_id` bigint(20) unsigned DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `passwort`
-ADD CONSTRAINT pk_passwort PRIMARY KEY (id);
+ADD CONSTRAINT pk_passwort PRIMARY KEY (id),
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
 
 -- --------------------------------------------------------
 
@@ -217,8 +153,7 @@ ADD CONSTRAINT pk_passwort PRIMARY KEY (id);
 -- Tabellenstruktur für Tabelle `projekt`
 --
 
-DROP TABLE IF EXISTS `projekt`;
-CREATE TABLE IF NOT EXISTS `projekt` (
+CREATE TABLE `projekt` (
 `id` bigint(20) unsigned NOT NULL,
   `projektname` varchar(128) NOT NULL,
   `salt` varchar(8) NOT NULL,
@@ -227,7 +162,8 @@ CREATE TABLE IF NOT EXISTS `projekt` (
 
 ALTER TABLE `projekt`
 ADD CONSTRAINT pk_projekt PRIMARY KEY (id),
-ADD CONSTRAINT unique_projektname UNIQUE (projektname);
+ADD CONSTRAINT unique_projektname UNIQUE (projektname),
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
 
 CREATE INDEX index_projektname ON `projekt` (projektname);
 
@@ -237,65 +173,19 @@ CREATE INDEX index_projektname ON `projekt` (projektname);
 -- Tabellenstruktur für Tabelle `sensor`
 --
 
-DROP TABLE IF EXISTS `sensor`;
-CREATE TABLE IF NOT EXISTS `sensor` (
+CREATE TABLE `sensor` (
 `id` bigint(20) unsigned NOT NULL,
   `sensorname` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `sensor`
 ADD CONSTRAINT pk_sensor PRIMARY KEY (id),
-ADD CONSTRAINT unique_sensorname UNIQUE (sensorname);
+ADD CONSTRAINT unique_sensorname UNIQUE (sensorname),
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
 
 
 -- ----------------------------------------
 -- ----------------------------------------
-
---
--- AUTO_INCREMENT für Tabelle `Messreihen`
---
-ALTER TABLE `anhang`
-MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `datentyp`
---
-
-ALTER TABLE `datentyp`
-MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `messreihe`
---
-ALTER TABLE `messreihe`
-MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `metainfo`
---
-ALTER TABLE `metainfo`
-MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `passwort`
---
-ALTER TABLE `passwort`
-MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `projekt`
---
-ALTER TABLE `projekt`
-MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `sensor`
---
-ALTER TABLE `sensor`
-MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
-
--- --------------------------------------------------------
--- --------------------------------------------------------
 
 -- 
 -- Foreign keys der exportierten Tabellen
