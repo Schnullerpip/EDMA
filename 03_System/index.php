e20dc10bca156df51fd358ab75c2f2a83a9198c2
Sandro Tonon <tonon.san@gmail.com>
2015-03-13 11:59:07 +0100

    Spinner Help Wanted von @phwachte
    Fixes #39
    HTML Markup cleaning

--- 03_System/index.php
+++ 03_System/index.php
@@ -280,7 +280,12 @@ $jsonselectsensor = json_encode($selectsensor);
 </div>
 
 
-<div id="jqChart-wrapper" style="width: 100%; height: 800px; display:none" data-title="<?php echo escape($projekt->data()->projektname); ?>"></div>
+<div class="row">
+    <div class="col-xs-12">
+        <div id="jqChart-wrapper" style="width: 100%; height: 800px; display:none" data-title="<?php echo escape($projekt->data()->projektname); ?>"></div>
+    </div>
+</div>
+
 
 <a id="saveImg" style="display:none" class="btn btn-default" href="#">Speichern als Bild</a>
 <a id="saveCSV" style="display:none" class="btn btn-default" target=_blank href="../datagross.csv">Speichern als CSV</a>
@@ -1347,11 +1352,11 @@ $jsonselectsensor = json_encode($selectsensor);
             $("#saveCSV").show();
             
             // ausblenden bei erneutem drücken von 'Anzeigen', solange Spinner eingeblendet ist
-//            if ($("#jqChart-wrapper").is(":visible")){
-//                $("#jqChart-wrapper").toggle();
-//                $("#saveImg").toggle();
-//                $("#saveCSV").toggle();
-//            }
+            if ($("#jqChart-wrapper").is(":visible")){
+                $("#jqChart-wrapper").toggle();
+                $("#saveImg").toggle();
+                $("#saveCSV").toggle();
+            }
 
             //die erste y-Achse (auf der linken Seite des Graphen) sollte zoom-enabled haben
             for (i = 0; i < scalas_copy.length; i++) {
@@ -1437,9 +1442,9 @@ $jsonselectsensor = json_encode($selectsensor);
             $('.loading-div, #anzeigeButton').toggle();
             
             // anzeigen, da Chart fertig geladen
-//            $("#jqChart-wrapper").toggle();
-//            $("#saveImg").toggle();
-//            $("#saveCSV").toggle();
+            $("#jqChart-wrapper").toggle();
+            $("#saveImg").toggle();
+            $("#saveCSV").toggle();
 
             $('#jqChart-wrapper').bind('tooltipFormat', function (e, data) {
                 var result = "<b>Zeitpunkt: ";
